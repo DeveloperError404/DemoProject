901b1b6ad1f27009ce787f8f9e4f05183fe84a96
Shiv <devendraspatil1995@gmail.com>
2024-06-17 16:35:21 +0530

    commit for Create  user Api

---
+++ src/main/java/com/example/service/UserServiceI.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import com.example.model.User;
+
+import java.util.List;
+
+public interface UserServiceI {
+    //CURD
+
+    //CREATE
+    User createUser(User user);
+
+    //UPDATE
+    User updateUser(User user, Long userId);
+
+
+    //GET(RETRIVE) single data
+    User getSingleUser(Long userId);
+
+
+    //GET(RETRIVE) all data
+List<User> getAllUsers();
+
+    //DELETE
+
+    void deleteUser(Long userId);
+}
---
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.service.impl;
+
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import com.example.service.UserServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserServiceI {
+    @Autowired
+    private UserRepository userRepository;                 //Field DI
+
+
+
+
+    @Override
+    public User createUser(User user) {
+       User savedUser = userRepository.save(user);
+        return savedUser;
+    }
+
+    @Override
+    public User updateUser(User user, Long userId) {
+        return null;
+    }
+
+    @Override
+    public User getSingleUser(Long userId) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+
+    }
+}
