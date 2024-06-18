45ad15201bbaf31e2f228a3eb6349a1325612109
Shiv <devendraspatil1995@gmail.com>
2024-06-18 11:09:19 +0530

    commit for @PostMappin in UserController

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.controller;
 import com.example.model.User;
 import com.example.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Method;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,10 +17,14 @@ public class UserController {
 
     @Autowired
     private UserServiceI userServiceI;
-
-
-    @PostMapping("/")
-    public ResponseEntity<User> createUser(){
-return null;
+//@PathVariable - pass single data
+    //@RequestBody - wholeobject- secure
+//@Requestparam -when we pass data in keyvalue pair like pagination and sorting
+    @PostMapping("/users")
+    //@RequestMapping(Method = RequestMethod.POST, name = "/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userServiceI.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);    //201 RESPONCE CREATED
     }
+
 }
